665a11858b9655ac16f5569ee318dd5f75426071
Piotr Rynkowski <46453195+Piter920120@users.noreply.github.com>
2021-12-02 11:03:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
@@ -18,13 +19,67 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
-
+        ArrayList<String>  lista1 = new ArrayList<>();
+        dodajZwierze(lista1);
+        zlapZwierze(lista1);
+        sortujZwierze(lista1);
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        Set<Integer> numery = new TreeSet<>();
+        dodajNumer(numery);
+        sortujNumer(numery);
+        zlapNumer(numery);
 
     }
 
+    public static void dodajZwierze(List<String> lista1){
+        Scanner scanner = new Scanner(System.in);
+        for ( int i = 0 ; i < 6; i++)
+        {
+            System.out.println("podaj zwierze : ");
+            String zwierze = scanner.nextLine();
+            lista1.add(zwierze);
+        }
+
+
+    }
+
+    public static void zlapZwierze(List<String> lista1){
+        System.out.println("\nZwierze : ");
+        for(String zmienna : lista1){
+            System.out.print(zmienna);
+        }
+    }
+
+    public static void sortujZwierze(List<String>lista1){
+        Collections.sort(lista1, Collections.reverseOrder());
+    }
+
+
+    public static void dodajNumer(Set<Integer> numery)
+    {
+        Scanner scanner = new Scanner(System.in);
+        for(int i  = 0; i < 10; i++){
+            System.out.println("Podaj numer: ");
+            int numer = scanner.nextInt();
+            numery.add(numer);
+        }
+    }
+
+    public static void sortujNumer(Set<Integer> numery)
+    {
+        Iterator<Integer> i = numery.iterator();
+        while (i.hasNext());
+        i.next();
+    }
+
+    public static void zlapNumer(Set<Integer> numery){
+        System.out.println("\nLiczby: ");
+        for(Integer a: numery){
+            System.out.println(a);
+        }
+    }
 }
