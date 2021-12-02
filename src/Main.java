8f84ec2fc7aa091b7b57d43f7521cc7706e8be1a
Piotr Rynkowski <46453195+Piter920120@users.noreply.github.com>
2021-12-02 10:27:35 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,7 +12,10 @@ public class Main {
         b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 20,
         c) napisać metodę, która wypisze otrzymane wartości,
         d) wstawić wywołanie metody do metody main
-        */
+        */  int tablica1 = 21;
+            int tab1[] = new int[tablica1];
+            dokonczTablice(tab1);
+            wyswietlTablice(tab1);
 
         /* zad.2
         a) stworzyc tablicę typu String[6],
@@ -19,6 +23,10 @@ public class Main {
             (użyć do tego pętli i klasy Scanner),
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
+
+        String[] tablica2 = new String[6];
+        dokonczTablice2(tablica2);
+        wyswietlTablice2(tablica2);
 
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
@@ -29,6 +37,12 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+            String imie = "Piotr";
+            String nazwisko = "Rynkowski";
+            int wiek = 29;
+            wyswietlDane(imie);
+            wyswietlDane(imie + nazwisko);
+            wyswietlDane(imie + nazwisko + wiek);
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
             coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
@@ -37,7 +51,77 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int sumaA = dodajSume(a,b);
+        int sumaB = dodajSume(a , b, c);
 
+        System.out.println("Suma wywołań obu metod: " + dodajSume(a,b) + " + " + dodajSume(a,b,c));
+    }
+
+    public static int[] dokonczTablice(int[] tab1)
+    {
+        int j = 0;
+        for (int i = 40; i  >= 20; i-- )
+        {
+            tab1[j] = i;
+            System.out.print(tab1[i] + " ");
+            j++;
+            if(j > tab1.length)
+                break;
+        }
+        return tab1;
+    }
+
+    public static void wyswietlTablice(int[] tab1) {
+        for (int i = 0; i < tab1.length; i++)
+            System.out.print(tab1[i] + " ");
+    }
+
+    public static String[] dokonczTablice2( String[] tablica2)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int wyraz = 1;
+        for(int i = 0; i < tablica2.length; i++)
+        {
+            System.out.println("Podaj wyraz nr " + wyraz + "i wcisnij enter" );
+            tablica2[i] = scanner.nextLine();
+            wyraz++;
+        }
+        return  tablica2;
+    }
+        public static void wyswietlTablice2(String[] tablica2)
+        {
+            System.out.println(" Słowa w odwrotnej kolejnosci : ");
+                    for(int i = tablica2.length; i >= 0; i--)
+                    {
+                        System.out.print(tablica2[i]);
+                    }
+        }
+
+        public static void wyswietlDane(String imie)
+        {
+            System.out.println("podaj imie " + imie);
+        }
+    public static void wyswietlDane(String imie, String nazwisko)
+    {
+        System.out.println("podaj imie  " + imie + " nazwisko " + nazwisko);
+    }
+
+    public static void wyswietlDane(String imie, String nazwisko, int wiek)
+    {
+        System.out.println("podaj imie " + imie + " nazwisko "+nazwisko + "wiek " + wiek);
+    }
+
+
+    public static int dodajSume(int a , int b)
+    {
+        return a + b;
+    }
+    public static int dodajSume(int a, int b, int c)
+    {
+        return a + b + c;
     }
 
 }
